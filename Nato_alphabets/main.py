6e86fe7feb9eb3f93bc67799b4e5c585fdc0659a
IllegalCanteen <kaustubh.sunderraman@gmAIL.COM>
2025-12-19 09:58:42 +0530

    Update main.py

--- Nato_alphabets/main.py
+++ Nato_alphabets/main.py
@@ -4,11 +4,14 @@ nato_file=pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_words=pandas.DataFrame(nato_file)
 nato_words_dict={row.letter:row.code for (index,row) in nato_words.iterrows()}
 print(nato_words_dict)
-
-word=input("Enter a word: ").upper()
-try:
-    result=[nato_words_dict[letter] for letter in word]
-    print(result)
-except KeyError:
-    print("Error please enter a singular word with only letters :) ")
+def generate_word():
+    word=input("Enter a word: ").upper()
+    try:
+        result=[nato_words_dict[letter] for letter in word]
+    except KeyError:
+        print("Error please enter a singular word with only letters :) ")
+        generate_word()
+    else:
+        print(result)
+generate_word()
 
